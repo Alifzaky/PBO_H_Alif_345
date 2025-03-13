1ccf7295ba49b0444e1c1857554cc66561e7007f
pp <nasywa.muhammad70@gmail.com>
2025-03-13 08:17:19 +0700

    update tugas (class,method,attribute)

--- Prak.PBO/Prak.PBO_tugas/src/tugas1.java
+++ Prak.PBO/Prak.PBO_tugas/src/tugas1.java
@@ -1,11 +1,45 @@
 import java.util.Scanner;
+
+class Admin{
+    String username, password;
+
+    public void login() {
+        if (username.equals("Admin345") && password.equals("Password345")) {
+            System.out.println("login admin berhasi");
+        } else {
+            System.out.println("Login gagal! Username atau password salah");
+        }
+    }
+}
+
+class Mahasiswa {
+    String username, password;
+
+    public void login(){
+        if(username.equals("Alif Zaky Nasywa Muhammad") && password.equals("202410370110345")){
+            System.out.println("login mahasiswa berhasi");
+            displayInfo();
+        }else{
+            System.out.println("Login gagal! nama atau nim salah");
+        }
+    }
+
+    public void displayInfo(){
+        System.out.printf("Nama : %s \n", username);
+        System.out.printf("NIM  : %s"   , password);
+    }
+}
+
 public class tugas1 {
     public static void main(String[] args) {
         int role;
         String username, password;
-        Scanner inputRole = new Scanner(System.in);
+        Scanner inputRole     = new Scanner(System.in);
         Scanner inputUsername = new Scanner(System.in);
         Scanner inputPassword = new Scanner(System.in);
+
+        Admin objekAdmin   = new Admin();
+        Mahasiswa objekMhs = new Mahasiswa();
 
         System.out.println("Pilih Login : ");
         System.out.println("1. Admin");
@@ -17,27 +51,21 @@ public class tugas1 {
                 System.out.println("Login admin");
                 System.out.print("Masukkan username : ");
                 username = inputUsername.nextLine();
+                objekAdmin.username = username;
                 System.out.print("Masukkan password : ");
                 password = inputPassword.nextLine();
-                if(username.equals("Admin345") && password.equals("Password345")){
-                    System.out.println("login admin berhasi");
-                }else{
-                    System.out.println("Login gagal! Username atau password salah");
-                }
+                objekAdmin.password = password;
+                objekAdmin.login();
                 break;
             case 2:
                 System.out.println("Login mahasiswa");
                 System.out.print("Masukkan username : ");
                 username = inputUsername.nextLine();
+                objekMhs.username = username;
                 System.out.print("Masukkan password : ");
                 password = inputPassword.nextLine();
-                if(username.equals("Alif Zaky Nasywa Muhammad") && password.equals("202410370110345")){
-                    System.out.println("login mahasiswa berhasi");
-                    System.out.printf("Nama : %s \n", username);
-                    System.out.printf("NIM  : %s", password);
-                }else{
-                    System.out.println("Login gagal! nama atau nim salah");
-                }
+                objekMhs.password = password;
+                objekMhs.login();
                 break;
             default:
                 System.out.println("Pilihan tidak valid");
